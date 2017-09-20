6d2b8b9ccf4b23ba0639a7a54066f6a97b3a7a99
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-20 16:46:50 +0100

    Add geolocation support via geo-location API

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,15 @@ socket.on('newMessage', function (message) {
     console.log('New message', message);
     var li = $('<li></li>');
     li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
 
     $('#messages').append(li);
 });
@@ -25,5 +34,21 @@ $('#message-form').on('submit', function (e) {
         text: $('[name=message]').val()
     }, function () {
 
+    });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function (e) {
+        alert('Unable to fetch location');
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -13,3 +13,19 @@ describe('generateMessage', () => {
         expect(message).toInclude({ from, text });
     });
 });
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Location Admin';
+        var latitude = 123;
+        var longitude = 456;
+        var url = 'https://www.google.com/maps?q=123,456';
+
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(locationMessage.from).toBe(from);
+        expect(locationMessage.url).toBe(url);
+
+        expect(locationMessage).toInclude({ from, url });
+    });
+})
