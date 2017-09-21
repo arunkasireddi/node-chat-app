e217c87f4fff3be42d3eb46a23681493319fae5a
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-21 11:51:51 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -12,31 +30,31 @@ socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = $('#message-template').html()
     var html = Mustache.render(template, {
-        text: message.text, 
+        text: message.text,
         from: message.from,
         createdAt: formattedTime
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = $('#location-message-template').html();
     var html = Mustache.render(template, {
-        url: message.url, 
+        url: message.url,
         from: message.from,
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
-
     var messageTextBox = $('[name=message]');
-
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextBox.val()
