37520efaec53984e2484e4ce2979605497759f67
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-21 10:58:52 +0100

    Added formatted timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,4 @@
+const moment = require('moment');
+
+var date = moment();
+console.log(date.format('HH:MM A'));
--- public/js/index.js
+++ public/js/index.js
@@ -9,16 +9,18 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('New message', message);
     var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a = $('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
 
